8b361054c64a1d57dab5e9a3bb1124e00e6a3646
Divy484 <98522523+Divy484@users.noreply.github.com>
2023-07-25 21:03:55 +0530

    Practical 1

---
+++ src/main/kotlin/test.kt
@@ -0,0 +1,9 @@
+class Student (var er_no : Int, var name : String) {
+
+}
+
+fun main () {
+    val a = Student (72, "Divy")
+    println("Enrollment No.: "+a.er_no)
+    println("Name : "+a.name)
+}
